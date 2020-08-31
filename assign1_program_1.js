205fd44d00713a16cf5a30d8be6a5f1a87be6cce
vishalpatil113 <vishalp755@gmail.com>
2020-08-31 11:36:59 +0530

    Delete assign1_program_1.js
    deletet

--- assign1_program_1.js
+++
@@ -1,5 +0,0 @@
-function max(no1, no2, no3) {
-    var no = no1 > no2 ? (no1 > no3 ? 'no1: ' + no1 : 'no3: ' + no3) : (no2 > no1 ? 'no2 ' + no2 : 'no3' + no3);
-    return no;
-}
-console.log(max(23, 89, 6));
